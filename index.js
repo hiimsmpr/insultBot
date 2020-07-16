dffa08b37788e6ce4e6a3e7d79fdb3bc0bd1edc7
Glenn Brown <gcb8m8p>
2020-07-16 12:28:48 -0400

    new trigger words added

--- index.js
+++ index.js
@@ -6,21 +6,36 @@ const PREFIX = require("./shortcuts")
 client.once('ready', () => {
     console.log('Ready!');
 });
-
+let msgCounter = 0
+let randomCount = 10
 client.on('message', message => {
-    if (message.content.includes("!insult") || message.content.includes('Virtual')) {
-        
-       let insult 
-       axios.get(PREFIX.insultApiUrl).then(resp => {
-        message.channel.send(resp.data.insult);
+    lowerCasement = message.content.toLowerCase()
+    console.log(lowerCasement)
+    const user = message.author
+    // if (msgCounter === randomCount) {
+    //     axios.get(PREFIX.insultApiUrl).then(resp => {
+    //         message.channel.send(`${user} ` + resp.data.insult);
+    //     });
+
+    //     randomCount = Math.floor(Math.random() * 50)
+    //     msgCounter = 0
+    // }
+    // if (message.content.includes(" ")) {
+    //     msgCounter += 1
+    //     console.log(msgCounter)
+    //     console.log("random number is "+randomCount)
+    // }
+    if (lowerCasement.includes("!insult")||lowerCasement.includes("cranial rectalitis")||lowerCasement.includes("cranial rectalitis")||lowerCasement.includes("daphne")||lowerCasement.includes("blaze")||lowerCasement.includes("asshole")||lowerCasement.includes("cunt")) {
+
+        let insult
+        axios.get(PREFIX.insultApiUrl).then(resp => {
+            message.channel.send(`${user} ` + resp.data.insult);
             // console.log(resp.data.insult);
         });
         console.log(insult)
-        
+
     }
     if (message.content.includes('shut up bot')) {
-        const user = message.author
-
         message.channel.send(`blow me ${user}!!!`);
     }
 });
