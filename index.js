8be6d0f6ea3e1a92a4c6f5f76342e54d21988893
Glenn Brown <gcb8m8p>
2020-08-23 17:02:04 -0400

    possible bug fix

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const axios = require('axios');
 const client = new Discord.Client();
 const CONFIG = require("./configs")
 const PREFIX = require("./shortcuts")
+const { prefix } = require('./config.json');
 client.once('ready', () => {
     console.log('Ready!');
 });
 let msgCounter = 0
 let randomCount = 10
 client.on('message', message => {
+  
     lowerCasement = message.content.toLowerCase()
     const user = message.author
+    console.log(`${user}: ` + message.content)
     // if (msgCounter === randomCount) {
     //     axios.get(PREFIX.insultApiUrl).then(resp => {
     //         message.channel.send(`${user} ` + resp.data.insult);
@@ -24,19 +27,49 @@ client.on('message', message => {
     //     console.log(msgCounter)
     //     console.log("random number is "+randomCount)
     // }
-    if (lowerCasement.includes("!insult")||lowerCasement.includes("cranial rectalitis")||lowerCasement.includes("cranial rectalitis")||lowerCasement.includes("daphne")||lowerCasement.includes("blaze")||lowerCasement.includes("asshole")||lowerCasement.includes("cunt")) {
+    if (lowerCasement.includes("cranial rectalitis")||lowerCasement.includes("cranial rectalitis")||lowerCasement.includes("daphne")||lowerCasement.includes("blaze")||lowerCasement.includes("asshole")||lowerCasement.includes("cunt")) {
 
         let insult
         axios.get(PREFIX.insultApiUrl).then(resp => {
             message.channel.send(`${user} ` + resp.data.insult);
-            // console.log(resp.data.insult);
         });
-        console.log(insult)
-
     }
     if (message.content.includes('shut up bot')) {
         message.channel.send(`blow me ${user}!!!`);
     }
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'sutime') {
+        if (args.length < 2) {
+            message.channel.send("Please provide a distance and speed dumb ass")
+        } else {
+            const minuteConv = .0166667
+            let distanceToTravel = parseFloat(args[0]) * 200
+            let speed = parseInt(args[1])
+            let timeCalculated = distanceToTravel / speed
+            let timeCalculatedMinutes = Math.trunc(timeCalculated / minuteConv)
+            let timeInHours = Math.trunc(timeCalculatedMinutes / 60)
+            let timeInMinutes = timeCalculatedMinutes % 60
+            message.channel.send("Esitmated time: " + timeInHours + " hours and " + timeInMinutes + " minutes.")
+        }
+    }else if (command === 'insult') {
+        if (args.length === 1) {
+            console.log("if hit")
+            let insult
+            axios.get(PREFIX.insultApiUrl).then(resp => {
+                message.channel.send(resp.data.insult+args[0]);
+            });
+            return
+        } else {
+            console.log("else hit")
+            let insult
+            axios.get(PREFIX.insultApiUrl).then(resp => {
+                message.channel.send(`${user} ` + resp.data.insult);
+            });
+        }
+    }
+    
 });
 
 client.login(CONFIG.token);
